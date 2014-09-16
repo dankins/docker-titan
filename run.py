5d01e469fb9934cdfe828e2e127082e20b13d8b6
Dan Kinsley <dan@thinkingphones.com>
2014-09-16 16:39:31 -0400

    adding batch loading and autotype config options

--- run.py
+++ run.py
@@ -13,6 +13,8 @@ REXSTER_BIN = os.path.join(REXSTER_BASE,'bin/rexster.sh')
 REXSTER_CONFIG_FILE = os.path.join(REXSTER_BASE,'conf/rexster-cassandra.xml')
 REXSTER_BASE_URI = os.environ['REXSTER_BASE_URI']
 REXSTER_PORT = os.getenv('REXSTER_PORT','8182')
+AUTOTYPE = os.getenv('TITAN_AUTOTYPE','blueprints')
+BATCH = os.getenv('TITAN_BATCH','true')
 
 # These are the Cassandra seed nodes for the cluster
 # ideally this should come from etcd
@@ -32,6 +34,8 @@ for props in graph.iterfind('properties'):
 
 # set new properties
 props = ET.Element('properties')
+ET.SubElement(props,'autotype').text = AUTOTYPE
+ET.SubElement(props,'storage.batch-loading').text = BATCH
 ET.SubElement(props,'storage.backend').text = "cassandra"
 ET.SubElement(props,'storage.hostname').text = SEEDS
 ET.SubElement(props,'storage.port').text = "9160"
